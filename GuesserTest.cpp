93b5ab489093fbe4c4fff0bdb624840528b5288d
Elijah Beverley <ebeverley@csuchico.edu>
2026-04-29 12:05:18 -0700

    Q03 Submission had git issues which is why I am submitting late

--- GuesserTest.cpp
+++ GuesserTest.cpp
@@ -14,9 +14,54 @@ class GuesserTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
-// Example "smoke test" (can be deleted)
-TEST(GuesserTest, smoke_test)
+TEST(GuesserTest, Constructor_character)
+{
+  Guesser object("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+  ASSERT_EQ( object.distance("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"),0);
+}
+
+
+TEST(GuesserTest, distance_same_string)
 {
   Guesser object("Secret");
-  ASSERT_EQ( 1+1, 2 );
+  ASSERT_EQ( object.distance("Secret"),0);
 }
+
+TEST(GuesserTest, Counts_diiference)
+{
+  Guesser object("elijah");
+  ASSERT_EQ( object.distance("elibah"),1);
+}
+
+
+TEST(GuesserTest, Count_add_character)
+{
+  Guesser object("12345");
+  ASSERT_EQ( object.distance("1234567"),2);
+}
+
+TEST(GuesserTest, Special_character)
+{
+  Guesser object("");
+  ASSERT_EQ( object.distance("$$"),0);
+}
+TEST(GuesserTest, RepeatedSymbols)
+{
+  Guesser object("$$$b");
+  ASSERT_EQ( object.distance("$$b$"),2);
+}
+
+TEST(GuesserTest, Same_returns_true)
+{
+    Guesser object("burger");
+    ASSERT_TRUE(object.match("burger"));
+}
+
+TEST(GuesserTest, Incorrect_changes_remain)
+{
+    Guesser object("Warrior");
+    (object.match("Warriors"));
+	 ASSERT_FALSE(object.match("Warriors")); 
+}
+
+
